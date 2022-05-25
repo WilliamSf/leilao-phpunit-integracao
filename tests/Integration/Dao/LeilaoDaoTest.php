1e0bb4739dab8d5003d2bbbcc95d5c798c46e6cf
William Silva <william.faria@rarolabs.com.br>
2022-05-25 17:41:20 -0300

    usando testes com banco em memoria

--- tests/Integration/Dao/LeilaoDaoTest.php
+++ tests/Integration/Dao/LeilaoDaoTest.php
@@ -9,19 +9,46 @@ use PHPUnit\Framework\TestCase;
 
 class LeilaoDaoTest extends TestCase
 {
+    /** @var \PDO */
+    private static $pdo;
+
+    public static function setUpBeforeClass(): void
+    {
+        self::$pdo = new \PDO('sqlite::memory:');
+        self::$pdo->exec('create table leiloes (
+            id INTEGER primary key,
+            descricao TEXT,
+            finalizado BOOL,
+            dataInicio TEXT
+        );');
+    }
+
+    protected function setUp(): void
+    {
+        self::$pdo->beginTransaction();
+    }
+
     public function testInsercaoEBuscaDevemFuncionar()
     {
+        // arrange
         $leilao = new Leilao('Variante 0Km');
-        $leilaoDao = new LeilaoDao(ConnectionCreator::getConnection());
-
+        $leilaoDao = new LeilaoDao(self::$pdo);
         $leilaoDao->salva($leilao);
+
+        // act
         $leiloes = $leilaoDao->recuperarNaoFinalizados();
 
+        // assert
         self::assertCount(1, $leiloes);
         self::assertContainsOnlyInstancesOf(Leilao::class, $leiloes);
         self::assertSame(
-            'Viariante 0Km',
+            'Variante 0Km',
             $leiloes[0]->recuperarDescricao()
         );
+    }
+
+    protected function tearDown(): void
+    {
+        self::$pdo->rollBack();
     }
 }
